6a4aa7951de01b3139e371682578dc39cd922dc7
fleshascs <35168446+fleshascs@users.noreply.github.com>
2021-07-14 23:08:33 +0300

    cleanup

--- src/dataProvider/utils.ts
+++ src/dataProvider/utils.ts
@@ -1,18 +1,20 @@
 import { GetMatchesByFilters, MatchUpdates } from '../types/ggbetAPI';
 const fs = require('fs');
-const fsp = require('fs').promises;
+const sanitize = require('sanitize-filename');
 
 export function saveMatch(slug: string, match: MatchUpdates): void {
+  const safeSlug = sanitize(slug);
   const matchData = JSON.stringify(match, null, 2);
-  fs.writeFile(`./data/${slug}.json`, matchData, function (err) {
+  fs.writeFile(`./data/${safeSlug}.json`, matchData, function (err) {
     if (err) return console.log(err);
-    console.log(`matchData saved to > ${slug}.json`);
+    console.log(`matchData saved to > ${safeSlug}.json`);
   });
 }
 
 export async function getSavedMatch(slug: string): Promise<MatchUpdates | null> {
   try {
-    return JSON.parse(await fsp.readFile(`./data/${slug}.json`, 'utf8'));
+    const safeSlug = sanitize(slug);
+    return JSON.parse(await fs.promises.readFile(`./data/${safeSlug}.json`, 'utf8'));
   } catch (error) {
     return null;
   }
