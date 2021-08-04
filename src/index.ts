5efed288b4e7e3ab3e054e1301a8977e38b31a87
Karolis Kaseta <kkas@danskebank.lt>
2021-08-04 20:07:34 +0300

    aws

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Application = express();
 app.get('/', (_req: Request, res: Response) => {
   res.send('TS App is Running');
 });
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`server is running on PORT ${PORT}`);
 });
