86ed715e89419d34dac7715236064e64e5ad7f49
fleshascs <35168446+fleshascs@users.noreply.github.com>
2021-08-23 01:54:16 +0300

    fix tests

--- status-web/server/index.ts
+++ status-web/server/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import next from 'next';
 
+// https://levelup.gitconnected.com/set-up-next-js-with-a-custom-express-server-typescript-9096d819da1c
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
