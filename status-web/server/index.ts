e449ba2d9122c6504716405657dd83e6c29a71a6
fleshascs <35168446+fleshascs@users.noreply.github.com>
2021-09-07 22:41:42 +0300

    init status page

--- status-web/server/index.ts
+++ status-web/server/index.ts
@@ -1,9 +1,33 @@
+// import { createServer } from 'http'
+// import { parse } from 'url'
+// import next from 'next'
+
+// const port = parseInt(process.env.PORT || '3000', 10)
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url!, true)
+//     handle(req, res, parsedUrl)
+//   }).listen(port)
+
+//   // tslint:disable-next-line:no-console
+//   console.log(
+//     `> Server listening at http://localhost:${port} as ${
+//       dev ? 'development' : process.env.NODE_ENV
+//     }`
+//   )
+// })
+
 import express from 'express';
 import next from 'next';
 
 // https://levelup.gitconnected.com/set-up-next-js-with-a-custom-express-server-typescript-9096d819da1c
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+// const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -15,8 +39,8 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  server.listen(port, '0.0.0.0', () => {
+    // eslint-disable-next-line no-console
+    console.log(`server is running on PORT ${port}`);
   });
 });
