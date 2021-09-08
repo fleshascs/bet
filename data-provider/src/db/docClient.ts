5bff9cc82385b3b1e87a0aac54bf5aeb6f1ae7d1
fleshascs <35168446+fleshascs@users.noreply.github.com>
2021-09-08 18:59:40 +0300

    fix ci

--- data-provider/src/db/docClient.ts
+++ data-provider/src/db/docClient.ts
@@ -2,6 +2,8 @@ import AWS from 'aws-sdk';
 
 AWS.config.update({
   region: 'us-west-2',
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   endpoint: 'http://localhost:8000'
 });
 
